9ac25a1c805afabb86b8e9aaadd24bf71f0aa8f3
Petr Sahula <pedro@luhas.cz>
2024-02-13 10:25:41 +0000

    WIP: fetched pets data

--- main.js
+++ main.js
@@ -4,5 +4,13 @@ async function start() {
     const ourTemperature = weatherData.properties.periods[0].temperature
     document.querySelector('#temperature-output').textContent = ourTemperature
 }
-
 start()
+
+async function petsArea() {
+    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+    const petsData = await petsPromise.json()
+    petsData.forEach(pet => {
+        console.log(pet.name);
+    });
+}
+petsArea()
